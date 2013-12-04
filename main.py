0f3667c7f44bf5f039849b9d1350604de04c36d2
niclas <missingurchin@gmail.com>
2013-12-04 17:21:41 +0800

    codes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 from Tkinter import *
+import xml.etree.cElementTree as ET
+
 class EditButtons():
   def __init__(self, master):
     fream = Frame(master)
@@ -24,20 +26,35 @@ class AddGui():
     self.nameEntry = Entry(fream)
     self.nameEntry.pack(side = RIGHT)
 
+def getDateFromxml(user):
+     tree = ET.ElementTree(file='./data.xml')
+     root = tree.getroot()
+     owers = list()    #这里存放总的用户姓名
+     numbList = list()
+
+     for owerName in tree.iter(tag='owner'):
+       nameW = owerName.attrib['name']
+       owers.append(nameW)
+
+     if user in owers:
+       xpath = 'owner[@name=\"'+user+'\"]/name' #这里是用XPath写的
+       for elem in tree.iterfind(xpath):
+         #print elem.text,elem.attrib['number']
+         numbList.append((elem.text,elem.attrib['number']))
+     return numbList
+
 mainWin = Tk()
 
 lstBox = Listbox(mainWin)
 # 这里是ListBox的填充内容
-for i in range(1,9):
+listcont = getDateFromxml(user)
+lenFriend = len(getDateFromxml('niclas')) +1
+
+for i in range(1,lenFriend):
   lstBox.insert(i,i*"h")
 lstBox.pack()
 
-ed     = EditButtons(mainWin)
-
-#add    = AddGui(mainWin, 'name')
-#dele   = AddGui(mainWin, 'delete')
-#mod    = AddGui(mainWin, 'mod')
-#search = AddGui(mainWin, 'search')
+ed = EditButtons(mainWin)
 
 mainWin.title('Main')
 mainWin.geometry('200x300')
