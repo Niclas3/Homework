4295105232cdac94cbd4fcdd8c52c4a541569010
niclas <missingurchin@gmail.com>
2013-12-04 22:56:24 +0800

    codes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 from Tkinter import *
 import xml.etree.cElementTree as ET
+from login import whosLog
 
 class EditButtons():
   def __init__(self, master):
@@ -47,14 +48,15 @@ mainWin = Tk()
 
 lstBox = Listbox(mainWin)
 # 这里是ListBox的填充内容
-listcont = getDateFromxml('niclas')  # 这里传入从login里面获得的用户名
-lenFriend = len(getDateFromxml('niclas')) +1
+listcont = getDateFromxml(whosLog)  # 这里传入从login里面获得的用户名
+lenFriend = len(getDateFromxml(whosLog)) +1
 
 for i in range(1,lenFriend):
   lstBox.insert(i,listcont[i-1][0]+"\t"+listcont[i-1][1])
 lstBox.pack()
 
 ed = EditButtons(mainWin)
+print "whosLog: "+ whosLog + '\n'
 
 mainWin.title('Main')
 mainWin.geometry('200x300')
